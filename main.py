0ad70e64f9f4ff1e6f30d130fc5198d27300b9b3
Lennard Hansmann <lennardhansmann@gmx.de>
2023-04-16 22:24:19 +0200

    kleines dropdown zur aktienauswal eingefügt

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import yfinance as yf
 import dash
 from dash import dcc
 from dash import html
+from dash.dependencies import Input, Output
 import plotly.express as px
 
 # Erstellen Sie ein Objekt für das zu beobachtende Unternehmen
@@ -16,11 +17,19 @@ app = dash.Dash(__name__)
 app.layout = html.Div(
     children=[
         html.H1('Verlauf der Microsoft Aktie', style={'textAlign': 'center'}),
-        dcc.Graph(id='example-graph', figure=px.line(df, x="Date", y="Open"),style={'backgroundColor': '#000000'}),
-        dcc.Dropdown(options=[{'label': 'Microsoft', 'value': 'option1'},{"label": 'Netflix', "value": 'option2'},
-        {'label': 'Tesla', 'value': 'option3'}])
+        dcc.Graph(id="timeline", figure=px.line(df, x="Date", y="Open"),style={'backgroundColor': '#000000'}),
+        dcc.Dropdown(id="dropdown",options=[{'label': 'Microsoft', 'value': 'option1'},{"label": 'Netflix', "value": 'option2'},
+        {'label': 'Tesla', 'value': 'option3'}]),
+        html.Br(),
+        html.Div(id='my-output'),
 ])
 
+@app.callback(
+    Output(component_id='my-output', component_property='children'),
+    Input(component_id='dropdown', component_property='value')
+)
+def update_output_div(input_value):
+    return input_value
 
 # Starte die App
 if __name__ == '__main__':
