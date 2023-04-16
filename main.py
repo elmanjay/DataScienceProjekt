e79dd2af947b1b7c3f446a86e70a15efe08adeca
Lennard Hansmann <lennardhansmann@gmx.de>
2023-04-16 21:05:13 +0200

    Erstes Dashboard Implementiert

--- main.py
+++ main.py
@@ -1,7 +1,27 @@
 import pandas as pd
 import yfinance as yf
+import dash
+from dash import dcc
+from dash import html
+import plotly.express as px
 
 # Erstellen Sie ein Objekt für das zu beobachtende Unternehmen
 msft = yf.Ticker("MSFT")
 df= msft.history(period="max")
-print(df.head())
+df.reset_index(inplace= True)
+
+
+# Erstelle das Dash-Layout
+app = dash.Dash(__name__)
+app.layout = html.Div(
+    children=[
+        html.H1('Verlauf der Microsoft Aktie', style={'textAlign': 'center'}),
+        dcc.Graph(id='example-graph', figure=px.line(df, x="Date", y="Open"),style={'backgroundColor': '#000000'}),
+        dcc.Dropdown(options=[{'label': 'Microsoft', 'value': 'option1'},{"label": 'Netflix', "value": 'option2'},
+        {'label': 'Tesla', 'value': 'option3'}])
+])
+
+
+# Starte die App
+if __name__ == '__main__':
+    app.run_server(debug=True)
