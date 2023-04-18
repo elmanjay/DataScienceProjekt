91ef953afa7456ed8c8ea5ff957ab64795b379a9
Lennard Hansmann <lennardhansmann@gmx.de>
2023-04-18 20:05:35 +0200

    callbacks funktionieren jetzt im head und fig

--- main.py
+++ main.py
@@ -9,28 +9,36 @@ import plotly.express as px
 # Erstellen Sie ein Objekt für das zu beobachtende Unternehmen
 assets = ["AAPL", "GOOGL", "TSLA", "MSFT"]
 aktien = ["Amazon","Googel","Tesla","Microsoft"]
-msft = yf.Ticker("MSFT")
-df= msft.history(period="max")
-df.reset_index(inplace= True)
 
 
 # Erstelle das Dash-Layout
 app = dash.Dash(__name__)
 app.layout = html.Div(
     children=[
+        html.H1('Text', id="head", style={'textAlign': 'center'}),
         dcc.Dropdown(id='aktien-dropdown',options=[{'label': j, 'value': aktie} for j, aktie in zip(aktien, assets)],value=assets[0]),
-        html.H1('Verlauf der Microsoft Aktie', style={'textAlign': 'center'}),
-        dcc.Graph(id="timeline", figure=px.line(df, x="Date", y="Open"),style={'backgroundColor': '#000000'}),
-        html.Br(),
-        html.Div(id='my-output'),
-])
+        dcc.Graph(id="timeline")
+        ])
 
 @app.callback(
-    Output(component_id='my-output', component_property='children'),
-    Input(component_id='dropdown', component_property='value')
+    Output(component_id='timeline', component_property='figure'),
+    Input(component_id='aktien-dropdown', component_property='value')
 )
 def update_output_div(input_value):
-    return input_value
+    msft = yf.Ticker(input_value)
+    df= msft.history(period="max")
+    df.reset_index(inplace= True)
+    figure= px.line(df, x="Date", y="Open")
+    return figure
+
+#Bearbeitung des Heads
+@app.callback(
+    Output(component_id='head', component_property='children'),
+    Input(component_id='aktien-dropdown', component_property='value')
+)
+def update_output_div(input_value):
+    return 'Verlauf der {} Aktie'.format(input_value)
+    
 
 # Starte die App
 if __name__ == '__main__':
