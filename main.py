b1407a62a83136f683ec296e4916e67222f2204a
richkernel <109109730+richkernel@users.noreply.github.com>
2023-04-25 10:44:01 +0200

    Open geändert

--- main.py
+++ main.py
@@ -87,7 +87,7 @@ def update_output_div(input_value):
     msft = yf.Ticker(input_value)
     df= msft.history(period="max")
     df.reset_index(inplace= True)
-    figure= px.line(df, x="Date", y="Open", title="Verlauf der Aktie")
+    figure= px.line(df, x="Date", y="Kurs", title="Verlauf der Aktie")
     return figure
 
 #Bearbeitung des Heads entsprechend der ausgewählten Aktie
