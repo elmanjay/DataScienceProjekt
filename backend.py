b1d2d2d4a9109f76bbbb7c1bda1064dbddfd7f6d
elmanjay <107359233+elmanjay@users.noreply.github.com>
2023-05-15 15:50:25 +0200

    Delete backend.py

--- backend.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-import statsmodels.api as sm
-
-def decompose(df):
-    decomposition = sm.tsa.seasonal_decompose(df["Close"], model="multiplikativ", period=12)
-    df["Trend"] = decomposition.trend
-    df["Saison"] = decomposition.seasonal
-    df["Rauschen"] = decomposition.resid
-    return df
