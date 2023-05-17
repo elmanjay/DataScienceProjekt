96545effc008e435b0cc046b9b709f6b36ad67a3
Lennard Hansmann <lennardhansmann@gmx.de>
2023-05-17 16:15:18 +0200

    jetzt aber final

--- pages/info.py
+++ pages/info.py
@@ -81,7 +81,7 @@ layout = dbc.Container(
                                                          style={"margin-left": "10px"}),
                                             ],
                                             className="card border-primary mb-3",
-                                            style={"height": "100%"}
+                                            style={"height": "96%"}
                                         ),
                                     width=6),
                                 ],
@@ -102,7 +102,7 @@ layout = dbc.Container(
                                                 #html.Div(id="prognose", style={"margin-left": "10px"}),
                                             ],
                                             className="card text-white bg-primary mb-3",
-                                            style={"height": "96%"}
+                                            style={"height": "100%"}
                                         ),
                                     ),
                                 ],
